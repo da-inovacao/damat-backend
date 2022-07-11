1f94fc4cb661827eacd9953ed870c98bb94942ca
Cleianderson <cleianderson13@gmail.com>
2022-07-11 10:27:33 -0300

    refact: presentations update

--- app/Controllers/Http/EventsController.ts
+++ app/Controllers/Http/EventsController.ts
@@ -1,5 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Event from 'App/Models/Event'
+import Presentation from 'App/Models/Presentation'
 
 export default class EventsController {
   public async create({ auth, request }: HttpContextContract) {
@@ -29,6 +30,14 @@ export default class EventsController {
 
     const id = request.param('id')
     const data = request.body()
+    const presentations = request.input('presentations')
+
+    for(let pres of presentations){
+      const _pres = await Presentation.findOrFail(pres.id)
+      _pres.title = pres.title
+      // _pres.date = pres.date
+      _pres.save()
+    }
 
     const event = await Event.findOrFail(id)
     return await event.merge(data).save()
